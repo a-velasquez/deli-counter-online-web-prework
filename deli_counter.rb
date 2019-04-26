4aa211b7e31940b49918ccc550c20ac5546e000e
Alex Velasquez <jtinnesco@gmail.com>
2019-04-26 05:02:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def line(katz_deli)
     puts "The line is currently empty."
   else katz_deli.length > 0 
     index = 1 
-    katz_deli.each_with_index do |number, index| 
+    katz_deli.each_with_index do |name, index| 
       puts "The line is currently:""
   
 end
