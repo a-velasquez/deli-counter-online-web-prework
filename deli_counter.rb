de6bc9318fb7ed6d0497c8a014121ce2d06c3052
Alex Velasquez <jtinnesco@gmail.com>
2019-04-26 04:55:02 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,12 @@
 # Write your code here.
 katz_deli = []
 
+def line(katz_deli)
+  if katz_deli.size == 0 
+    puts "The line is currently empty"
+  end
+end
+
 def take_a_number (katz_deli, name)
   katz_deli.push(name)
   puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
