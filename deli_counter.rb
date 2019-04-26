685caae8d07d73a9892bc8233536c897a4e6d423
Alex Velasquez <jtinnesco@gmail.com>
2019-04-26 04:59:52 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,8 @@ def line(katz_deli)
   if katz_deli.length == 0 
     puts "The line is currently empty."
   else katz_deli.length > 0 
-    katz_deli.each_with_index ||
+    index = 1 
+    katz_deli.each_with_index |number, index|
   
 end
 
