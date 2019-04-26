58c49da81e1224b3d90da48e4842b21b7f1f6aa2
Alex Velasquez <jtinnesco@gmail.com>
2019-04-26 04:52:14 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,4 +3,5 @@ katz_deli = []
 
 def take_a_number (katz_deli, name)
   katz_deli.push(name)
-  puts "Welcome, #{name}. You are "
+  puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
+end
